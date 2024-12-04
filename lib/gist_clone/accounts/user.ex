e9d2382cb6632196e011f0a9abce5a1a2397a760
Murilo Azevedo <muriloazevedo338@gmail.com>
2024-12-04 17:03:01 -0300

    feat: create gists

--- lib/gist_clone/accounts/user.ex
+++ lib/gist_clone/accounts/user.ex
@@ -9,6 +9,7 @@ defmodule GistClone.Accounts.User do
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
     field :confirmed_at, :utc_datetime
+    has_many :gists, GistClone.Gists.Gist
 
     timestamps(type: :utc_datetime)
   end
--- lib/gist_clone_web/controllers/page_controller.ex
+++ lib/gist_clone_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule GistCloneWeb.PageController do
   use GistCloneWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home)
+    redirect(conn, to: "/create")
   end
 end
